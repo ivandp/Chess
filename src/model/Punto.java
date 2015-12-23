1fce91be506ac56f486620caf234cd114eecf4cd
ivandp <Ivan@MacBook-Pro-di-Ivan.local>
2015-12-23 11:25:26 +0100

    correct minor bugs

---
+++ src/model/Punto.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Punto {
+	private int x;
+	private int y;
+	public Punto(int x, int y){
+		this.x=x;
+		this.y=y;
+	}
+	
+	public int getX(){
+		return x;
+	}
+	
+	public int getY(){
+		return y;
+	}
+	public Punto get(){
+		return this;
+	}
+
+}
